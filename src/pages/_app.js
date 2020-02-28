5022f2ec1ac01d3af81e1d51f32f98f6386950e8
Kleyson Morais <kleysondev@gmail.com>
2020-02-27 23:32:20 -0300

    Initial Commit

---
+++ src/pages/_app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// eslint-disable-next-line
+export default function MyApp({ Component, pageProps }) {
+  // eslint-disable-next-line
+  return <Component {...pageProps} />;
+}
