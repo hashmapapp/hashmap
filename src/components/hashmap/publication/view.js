4c81ec37295262cad8200a601a7c2431b4f88d72
kleyson <kleysondev@gmail.com>
2020-04-06 06:01:36 -0300

    Facebook Reactions

--- src/components/hashmap/publication/view.js
+++ src/components/hashmap/publication/view.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import ReactCount from 'app/components/hashmap/publication/components/react-count';
+import { Facebook } from './components/Facebook';
+// import ReactCount from 'app/components/hashmap/publication/components/react-count';
 // import WrapImage from 'app/components/UI/image/wrapper';
 // import LinkPreview from 'app/components/UI/link-preview/link-preview';
 // import AddReact from './components/add-react';
@@ -14,32 +15,8 @@ const Publication = ({ data }) => (
       <div className="font-sans font-bold text-xl mb-2">{data.title}</div>
       <p className="font-sans text-lg text-gray-700">{data.description}</p>
     </div>
-    <div className="px-6 pt-4 pb-2 grid grid-cols-9">
-      {data.react.like > 0 && (
-        <ReactCount value={data.react.like} type="like" />
-      )}
-      {data.react.heart > 0 && (
-        <ReactCount value={data.react.heart} type="heart" />
-      )}
-      {data.react.unlike > 0 && (
-        <ReactCount value={data.react.unlike} type="unlike" />
-      )}
-      {data.react.smiley > 0 && (
-        <ReactCount value={data.react.smiley} type="smiley" />
-      )}
-      {data.react.dissatisfied > 0 && (
-        <ReactCount value={data.react.dissatisfied} type="dissatisfied" />
-      )}
-      {data.react.rocket > 0 && (
-        <ReactCount value={data.react.rocket} type="rocket" />
-      )}
-      {data.react.hooray > 0 && (
-        <ReactCount value={data.react.hooray} type="hooray" />
-      )}
-      {data.react.eyes > 0 && (
-        <ReactCount value={data.react.eyes} type="eyes" />
-      )}
-      {/* <AddReact /> */}
+    <div className="px-6 pt-4 pb-2">
+      <Facebook />
     </div>
   </article>
 );
