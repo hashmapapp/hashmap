5946616e124067d5ad0e4a052df153687ee0ab9c
kleyson <kleysondev@gmail.com>
2020-04-26 15:56:38 -0300

    Version 1.0.0

--- src/screens/about/about.js
+++ src/screens/about/about.js
@@ -1,30 +1,5 @@
 import React from 'react';
-import UINavBar from 'app/components/UI/navbar/navbar';
-import { ItemLi } from 'app/components/UI/styles/styles';
-import Link from 'next/link';
 
-const about = () => (
-  <>
-    <UINavBar>
-      <ul>
-        <ItemLi>
-          <Link href="/edit">
-            <a>Criar HashMap</a>
-          </Link>
-        </ItemLi>
-        <ItemLi>
-          <Link href="/about">
-            <a>Sobre</a>
-          </Link>
-        </ItemLi>
-        <ItemLi>
-          <Link href="/view">
-            <a>Ler</a>
-          </Link>
-        </ItemLi>
-      </ul>
-    </UINavBar>
-  </>
-);
+const about = () => <div />;
 
 export default about;
