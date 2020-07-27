6a03fe2c6a81e80a6e439072766d1dae9f2f6a3e
Felipe Reis <felipereis.m.b@gmail.com>
2020-07-27 18:30:09 -0300

    Update view hashmap

--- src/components/hashmap/view.js
+++ src/components/hashmap/view.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import HeaderView from 'app/components/hashmap/header/view';
 import ArticleView from 'app/components/hashmap/article/view';
 import Footer from 'app/components/hashmap/footer/footer';
@@ -6,7 +6,11 @@ import CollaboratorsView from 'app/components/hashmap/collaborators/view';
 import PropTypes from 'prop-types';
 import ShareButtons from '../UI/share-buttons/share-buttons';
 
+import cx from 'classnames';
+
 const SectionHashmapView = ({ hashmap, posts }) => {
+  const [activeTab, setActiveTab] = useState(1);
+
   const headerData = {
     title: hashmap.title,
     subtitle: hashmap.subtitle,
@@ -29,10 +33,35 @@ const SectionHashmapView = ({ hashmap, posts }) => {
     <>
       <div className="container mx-auto lg:hidden">
         <HeaderView data={headerData} />
-        <ArticleView data={articleData} />
-        <ShareButtons title={hashmap.title} summary={hashmap.subtitle} />
-        <CollaboratorsView data={authorData} />
-        {/* <Footer data={authorData} /> */}
+        <ul className="flex border-b">
+          <li className="flex-1 mr-2 ml-2" onClick={() => setActiveTab(1)}>
+            <a className={cx("text-center block py-2 px-4",
+              { 'border-l border-t border-r rounded-t font-bold': activeTab === 1 }
+            )}>
+              Publicação
+            </a>
+          </li>
+          <li className="flex-1 mr-2" onClick={() => setActiveTab(2)}>
+            <a className={cx("text-center block py-2 px-4",
+              { 'border-l border-t border-r rounded-t font-bold': activeTab === 2 }
+            )}>
+              Settings
+            </a>
+          </li>
+        </ul>
+        {activeTab === 1 &&
+          <>
+            <ArticleView data={articleData} />
+            <ShareButtons title={hashmap.title} summary={hashmap.subtitle} />
+            <CollaboratorsView data={authorData} />
+          </>
+        }
+
+        {activeTab === 2 &&
+          <>
+            Settings Solicitar Colaboração
+          </>
+        }
       </div>
 
       <div className="container mx-auto px-24 hidden lg:flex">
